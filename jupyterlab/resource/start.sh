ed554a3e0233794ffa04d40eaceed6aa3b37cf01
Hang <64747715+xx529@users.noreply.github.com>
2022-09-06 17:58:18 +0800

    Update start.sh

--- jupyterlab/resource/start.sh
+++ jupyterlab/resource/start.sh
@@ -1,3 +1,3 @@
 /etc/init.d/ssh start
 nohup code-server >/dev/null 2>log &
-jupyter lab --ip 0.0.0.0 --no-browser --port=8888 --allow-root --NotebookApp.token=hang >/dev/null 2>log
+jupyter lab --ip 0.0.0.0 --no-browser --port=8888 --allow-root --NotebookApp.token=hang
